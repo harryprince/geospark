9233ad103e42d251fd84716072211d750c2cd231
harryzhu <harryzhu@mobike.com>
2019-01-31 15:09:12 +0800

    publish first version

--- R/dependencies.R
+++ R/dependencies.R
@@ -2,11 +2,22 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
   sparklyr::spark_dependency(
     jars = c(
       system.file(
-        sprintf("java/geospark-%s-%s.jar", spark_version, scala_version),
+        sprintf("java/geospark-sql_%s-%s.jar", spark_version, "1.1.3"),
         package = "geospark"
+      ),
+      system.file(
+          "java/geospark-1.1.3.jar",
+          package = "geospark"
+      ),
+      system.file(
+          "java/geospark-viz-1.1.3.jar",
+          package = "geospark"
       )
     ),
     packages = c(
+        sprintf("org.datasyslab:geospark-sql_%s:1.1.3",spark_version),
+        "com.vividsolutions:jts-core:1.14.0",
+        "org.datasyslab:geospark:1.1.3"
     )
   )
 }
--- R/gis_register.R
+++
@@ -1,9 +0,0 @@
-#' @examples
-#' \dontrun{
-#' register_gis(sc)
-#' }
-#' @export
-register_gis <- function(sc) {
-        sql_context <- invoke_new(sc, "org.apache.spark.sql.SQLContext", spark_context(sc))
-        invoke_new(sql_context,"org.datasyslab.geosparksql.utils.GeoSparkSQLRegistrator","registerAll")
-}
