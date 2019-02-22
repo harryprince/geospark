0dfddf018898e5bd4e88056e55ae5e6041c38e23
Javier Luraschi <javierluraschi@hotmail.com>
2019-02-21 18:55:21 -0800

    add support for livy and autoinitialize

--- R/dependencies.R
+++ R/dependencies.R
@@ -2,23 +2,29 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
   sparklyr::spark_dependency(
     jars = c(
       system.file(
-        sprintf("java/geospark-sql_%s-%s.jar", spark_version, "1.1.3"),
+        sprintf("java/geospark-sql_%s-%s.jar",
+                sparklyr::spark_dependency_fallback(spark_version, c("2.1", "2.2", "2.3")),
+                "1.1.3"),
         package = "geospark"
       ),
       system.file(
-          "java/geospark-1.1.3.jar",
-          package = "geospark"
+        "java/geospark-1.1.3.jar",
+        package = "geospark"
       ),
       system.file(
-          "java/geospark-viz-1.1.3.jar",
-          package = "geospark"
+        "java/geospark-viz-1.1.3.jar",
+        package = "geospark"
       )
     ),
     packages = c(
-        sprintf("org.datasyslab:geospark-sql_%s:1.1.3",spark_version),
-        "com.vividsolutions:jts-core:1.14.0",
-        "org.datasyslab:geospark:1.1.3"
-    )
+      sprintf("org.datasyslab:geospark-sql_2.3:1.1.3"),
+      "com.vividsolutions:jts-core:1.14.0",
+      "org.datasyslab:geospark:1.1.3"
+    ),
+    initializer = function(sc, ...) {
+      register_gis(sc)
+    },
+    web_jars = "https://github.com/javierluraschi/geospark/blob/master/inst/java/%s?raw=true"
   )
 }
 
