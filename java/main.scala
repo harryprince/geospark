e26d3d0f07228c07cdeb1ed31e337d2e74c2e716
Harry Zhu <harryzhu@mobike.com>
2019-05-03 08:01:03 +0800

    remove redudant

--- java/main.scala
+++
@@ -1,9 +0,0 @@
-package geospark
-
-object Main {
-  // def register_gis(spark: SparkSession) = {
-     // GeoSparkSQLRegistrator.registerAll(spark)
- // spark.udf.register("hello", (name: String) => {
- //   "Hello, " + name + "! - From Scala"
- // })
-}
