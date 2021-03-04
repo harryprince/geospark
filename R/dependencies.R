579e3ba5a435d43996a2e5e7633cacd54eef29aa
sllynn <stuart@databricks.com>
2021-03-04 13:59:36 +0000

    add spark_dependency_fallback catch back

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,7 +1,7 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
   sparklyr::spark_dependency(
     packages = c(
-      paste0("org.apache.sedona:sedona-python-adapter-",spark_version,"_",scala_version,":1.0.0-incubating"),
+      paste0("org.apache.sedona:sedona-python-adapter-",spark_dependency_fallback(spark_version, c("3.0")),"_",scala_version,":1.0.0-incubating"),
       "org.datasyslab:geotools-wrapper:geotools-24.0"
     ),
     initializer = function(sc, ...) {
